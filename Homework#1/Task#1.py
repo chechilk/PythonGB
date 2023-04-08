421662ebeca276089628f0e7a3caa45cb0547b07
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-04-08 17:15:42 +0300

    Задача 1. Сумма элементов трехзначного числа.

--- Homework#1/Task#1.py
+++ Homework#1/Task#1.py
@@ -1,1 +1,22 @@
+# Найдите сумму цифр трехзначного числа
+number = int(input('Введите трёхзначное число: '))
+def checkSize(number):
+    count = 0
+    while number > 0:
+        count += 1
+        number //= 10
+    return count
+def checkSumm(number):
+    summ = 0
+    if checkSize(number) == 3:
+        while number > 0:
+            summ = summ + (number % 10)
+            number //= 10
+    else:
+        print('Введите верное число')
+        exit()
+    return summ
+
+print(f'Сумма элементов в числе {number} = {checkSumm(number)}')
+
 
