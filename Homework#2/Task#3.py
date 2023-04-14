b5fcd5c75a8c8194121a4af66240fca51e2ab32d
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-04-14 07:39:46 +0300

    Задача 14. Числа второго порядка.

---
+++ Homework#2/Task#3.py
@@ -0,0 +1,24 @@
+''''## **Task#3**
+**Условие:** Требуется вывести все целые степени двойки (т.е. числа
+вида 2k), не превосходящие числа N.
+
+*Пример:*
+* 10 -> 1 2 4 8'''
+
+list_number = [1]
+count_number = int(input('Введите количество чисел: '))
+numbers = 0
+for i in range(1, count_number):
+    numbers = list_number[i - 1] + list_number[i - 1]
+    if numbers <= 10:
+        list_number.insert(i, numbers)
+    else:
+        print(list_number)
+        exit()
+print(list_number)
+
+
+
+
+
+
