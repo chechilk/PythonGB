ff2e1af2fca17b2b229903164c90df64e16dfffd
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-04-22 01:51:50 +0300

    Задача 22

---
+++ Homework#4/Task#22.py
@@ -0,0 +1,23 @@
+# Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа:
+# n — кол-во элементов первого множества;
+# m — кол-во элементов второго множества;
+# Затем пользователь вводит сами элементы множеств.
+import random
+
+len_first = int(input('Enter first length set: '))
+len_second = int(input('Enter second length set: '))
+list_first = list()
+list_second = list()
+
+for i in range(len_first):
+    list_first.append(random.randint(0, len_first))
+for i in range(len_second):
+    list_second.append(random.randint(0, len_second))
+
+set_first = set(list_first)
+set_second = set(list_second)
+print("First set: ", set_first)
+print("Second set: ", set_second)
+print("Intersection first set and second set: ", set_first.intersection(set_second))
